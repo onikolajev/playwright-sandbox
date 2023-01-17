6bf80008b7da49cf4e634dba9953d6219b7f34a3
onikolajev <olegoleg17@gmail.com>
2023-01-17 23:27:44 +0200

    Added case for Dynamic Controls page

---
+++ page-objects/HerokuDynamicControlsPage.ts
@@ -0,0 +1,55 @@
+import { Locator, Page, expect } from "@playwright/test";
+
+export class HerokuDynamicControlPage {
+  readonly page: Page;
+  readonly checkboxButton: Locator;
+  readonly checkboxEle: Locator;
+  readonly inputButton: Locator;
+  readonly inputEle: Locator;
+
+  constructor(page) {
+    this.page = page;
+    this.checkboxButton = this.page.locator("#checkbox-example > button");
+    this.checkboxEle = this.page.locator("#checkbox");
+    this.inputButton = this.page.locator("#input-example > button");
+    this.inputEle = this.page.locator("#input-example > input[type=text]");
+  }
+
+  async assertSectionHeader(header: string) {
+    await this.page.locator(
+      `#content > div.example > h4:has-text("${header}")`
+    );
+  }
+
+  async assertCheckboxButtonText(buttonText: "Remove" | "Add") {
+    await expect(this.checkboxButton).toContainText(buttonText);
+  }
+
+  async clickOnCheckboxButton() {
+    await this.checkboxButton.click();
+  }
+
+  async assertCheckboxState(state: "Visible" | "Hidden") {
+    if (state === "Visible") {
+      await expect(this.checkboxEle).toBeVisible();
+    } else {
+      await expect(this.checkboxEle).toBeHidden();
+    }
+  }
+
+  async clickOnInputButton() {
+    await this.inputButton.click();
+  }
+
+  async assertInputState(state: "Disabled" | "Enabled") {
+    if (state === "Disabled") {
+      await expect(this.inputEle).toBeDisabled();
+    } else {
+      await expect(this.inputEle).toBeEnabled();
+    }
+  }
+
+  async assertInputButtonText(btnText: "Enable" | "Disable") {
+    await expect(this.inputButton).toContainText(btnText);
+  }
+}
---
+++ tests/heroku-dynamic-controls.spec.ts
@@ -0,0 +1,71 @@
+import { test, Page } from "@playwright/test";
+import { HerokuPage } from "../page-objects/HerokuPage";
+import { HerokuDynamicControlPage } from "../page-objects/HerokuDynamicControlsPage";
+
+test.describe("Check Dynamical Controls page", () => {
+  let page: Page;
+  let herokuPage: HerokuPage;
+  let herokuDynamicControlsPage: HerokuDynamicControlPage;
+
+  test.beforeEach(async ({ page }) => {
+    herokuPage = new HerokuPage(page);
+    herokuDynamicControlsPage = new HerokuDynamicControlPage(page);
+
+    await herokuPage.open();
+    await herokuPage.selectSection("Dynamic Controls");
+  });
+
+  test("Should open Dynamic Controls page, and check title and element's default state", async ({
+    page,
+  }) => {
+    await herokuDynamicControlsPage.assertSectionHeader("Dynamic Controls");
+    await herokuDynamicControlsPage.assertCheckboxState("Visible");
+    await herokuDynamicControlsPage.assertCheckboxButtonText("Remove");
+    await herokuDynamicControlsPage.assertInputState("Disabled");
+    await herokuDynamicControlsPage.assertInputButtonText("Enable");
+  });
+
+  test("Should hide checkbox, if button was pressed once", async ({ page }) => {
+    await herokuDynamicControlsPage.clickOnCheckboxButton();
+    await herokuDynamicControlsPage.assertCheckboxState("Hidden");
+  });
+
+  test("Should hide & show again checkbox, if button pressed twice", async ({
+    page,
+  }) => {
+    await herokuDynamicControlsPage.assertCheckboxState("Visible");
+    await herokuDynamicControlsPage.assertCheckboxButtonText("Remove");
+    await herokuDynamicControlsPage.clickOnCheckboxButton();
+
+    await herokuDynamicControlsPage.assertCheckboxState("Hidden");
+    await herokuDynamicControlsPage.assertCheckboxButtonText("Add");
+    await herokuDynamicControlsPage.clickOnCheckboxButton();
+
+    await herokuDynamicControlsPage.assertCheckboxState("Visible");
+    await herokuDynamicControlsPage.assertCheckboxButtonText("Remove");
+  });
+
+  test("Should enable input if button was pressed once", async ({ page }) => {
+    await herokuDynamicControlsPage.assertInputButtonText("Enable");
+    await herokuDynamicControlsPage.assertInputState("Disabled");
+    await herokuDynamicControlsPage.clickOnInputButton();
+
+    await herokuDynamicControlsPage.assertInputState("Enabled");
+    await herokuDynamicControlsPage.assertInputButtonText("Disable");
+  });
+
+  test("Should enable & disable input if button was pressed twice", async ({
+    page,
+  }) => {
+    await herokuDynamicControlsPage.assertInputState("Disabled");
+    await herokuDynamicControlsPage.assertInputButtonText("Enable");
+    await herokuDynamicControlsPage.clickOnInputButton();
+
+    await herokuDynamicControlsPage.assertInputState("Enabled");
+    await herokuDynamicControlsPage.assertInputButtonText("Disable");
+    await herokuDynamicControlsPage.clickOnInputButton();
+
+    await herokuDynamicControlsPage.assertInputState("Disabled");
+    await herokuDynamicControlsPage.assertInputButtonText("Enable");
+  });
+});
