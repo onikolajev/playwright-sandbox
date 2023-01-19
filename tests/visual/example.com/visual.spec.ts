1c8c4e73b4246ed627e4c4e7599e3107c26bb205
onikolajev <olegoleg17@gmail.com>
2023-01-19 21:30:20 +0200

    Visual testing test

--- tests/visual/example.com/visual.spec.ts
+++ tests/visual/example.com/visual.spec.ts
@@ -4,5 +4,11 @@ test.describe("Visual regression test", () => {
   test("Full page snashot", async ({ page }) => {
     await page.goto("https://www.example.com");
     expect(await page.screenshot()).toMatchSnapshot("homepage.png");
+  })
+  
+  test('Single Page Snapshot', async ({page})=>{
+    await page.goto("https://www.example.com");
+    const pageEle = await page.$('h1')
+    expect (await pageEle?.screenshot()).toMatchSnapshot('page-title.png')
   });
 });
